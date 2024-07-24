455693d1e651c66d5785b71fea7b53411d4ae92f
= <xodus1349@likelion.org>
2024-07-24 13:50:37 +0900

    0724

--- algo/src/baekjoon/Main_14500.java
+++ algo/src/baekjoon/Main_14500.java
@@ -16,17 +16,20 @@ public class Main_14500 {
 			result = Math.max(result, sum);
 			return;
 		}
-		v[x][y] = true;
 		for(int i=0;i<4;i++) {
 			int nx = dx[i]+x;
 			int ny = dy[i]+y;
-			if(0<=nx && nx<N && 0<=ny && ny<M) {
+			if(0<=nx && nx<N && 0<=ny && ny<M && !v[nx][ny]) {
+				v[nx][ny] = true;
+				if(count==2) {
+					dfs(x, y, count+1, sum+paper[nx][ny]);
+				}
 				dfs(nx, ny, count+1, sum+paper[nx][ny]);
+				v[nx][ny] = false;
 			}
-		}
-		
-		
+		}	
 	}
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -35,7 +38,7 @@ public class Main_14500 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		paper = new int[N][M];
-		v = new boolean[N][M];
+
 		result = 0;
 		
 		for(int i=0;i<N;i++) {
@@ -45,8 +48,14 @@ public class Main_14500 {
 			}
 		}
 		
-		
-		dfs(0, 0, 0, paper[0][0]);
+		v = new boolean[N][M];
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				v[i][j] = true;
+				dfs(i, j, 1, paper[i][j]);
+				v[i][j] = false;
+			}
+		}
 		System.out.println(result);
 	}
 		
