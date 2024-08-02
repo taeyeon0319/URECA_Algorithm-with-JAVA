739a72abf4dbc2c74593bea0bebec0e3ed948a9f
taeyeon0319 <xodus1349@likelion.org>
2024-08-02 17:51:54 +0900

    0802

--- algo/src/baekjoon/Main_9019.java
+++ algo/src/baekjoon/Main_9019.java
@@ -8,50 +8,48 @@ public class Main_9019 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=0;tc<T;tc++) {
-			st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
+			st =new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
 			String[] answer = new String[10000];
 			boolean[] visited = new boolean[10000];
 			
 			ArrayDeque<Integer> q = new ArrayDeque<>();
 			q.offer(A);
-			visited[A] = true;
-			Arrays.fill(answer, "");
+			visited[A]= true;
+			answer[A]="";
 			while(!q.isEmpty() && !visited[B]) {
 				int now = q.poll();
 				int D = (now*2)%10000;
-				int S = (now-1)==0?9999:(now-1);
+				int S = now==0?9999:(now-1);
 				int L = (now%1000)*10+(now/1000);
 				int R = (now%10)*1000+(now/10);
 				
-				if(!visited[D]) {
+				if (!visited[D]) {
 					q.offer(D);
-					answer[D] = answer[now]+"D";
 					visited[D]=true;
+					answer[D] = answer[now]+"D";
 				}
 				if(!visited[S]) {
 					q.offer(S);
-					answer[S] = answer[now]+"S";
 					visited[S]=true;
+					answer[S] = answer[now]+"S";
 				}
 				if(!visited[L]) {
 					q.offer(L);
-					answer[L] = answer[now]+"L";
 					visited[L]=true;
+					answer[L] = answer[now]+"L";
 				}
 				if(!visited[R]) {
 					q.offer(R);
-					answer[R] = answer[now]+"R";
 					visited[R]=true;
+					answer[R] = answer[now]+"R";
 				}
 			}
 			System.out.println(answer[B]);
 		}
+		
 	}
-
 }
-
