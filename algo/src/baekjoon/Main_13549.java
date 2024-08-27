7659cd1105b914431e576d059b7ff76820f4ea28
taeyeon0319 <xodus1349@likelion.org>
2024-08-27 13:49:29 +0900

    0827

---
+++ algo/src/baekjoon/Main_13549.java
@@ -0,0 +1,56 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_13549 {
+	static int N, K;
+	static int[] graph;
+	static boolean[] visited;
+	static int MAX=100001;
+	
+	public static void bfs(int a) {
+		visited[a] = true;
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		q.offer(a);
+		while(!q.isEmpty()) {
+			int current = q.poll();
+			if(current==K) {
+				return;
+			}
+			
+			int[] next = {current*2, current-1, current+1};
+			
+			for(int i=0;i<3;i++) {
+				int n = next[i];
+				if(n>=0 && n<MAX && !visited[n]) {
+					visited[n]=true;
+					if(i==0) {
+						graph[n] = graph[current];
+						q.addFirst(n);
+					}else {
+						graph[n] = graph[current]+1;
+						q.addLast(n);
+					}
+				}
+			}		
+		}
+
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		graph = new int[MAX];
+		visited = new boolean[MAX];
+		
+		bfs(N);
+		System.out.println(graph[K]);	
+		
+	}
+
+}
--- algo/src/baekjoon/Main_1865.java
+++ algo/src/baekjoon/Main_1865.java
@@ -3,7 +3,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main_1865 {
+	static int N, M, W;
+	static int[] dist;
+	static ArrayList<ArrayList<Road>> a;
+	static final int INF = 987654321;
 	
+	public class Road{
+		int end;
+		int weight;
+		
+		Road(int end, int weight){
+			this.end = end;
+			this.weight = weight;
+		}
+	}
+	//벨만포드 알고리즘
+	public static boolean bellmanFord(int start) {
+		Arrays.fill(dist,  INF);
+		dist[start] = 0;
+		boolean update = false;
+		
+		for(int i=1;i<N;i++) {
+			update=false;
+			
+			for(int j=1;j<=N;j++) {
+				for(Road road:a.get(j)) {
+					if(dist[j]!=INF && dist[road.end]>dist[j])
+				}
+//				https://steady-coding.tistory.com/91
+			}
+		}
+		return false;
+	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -15,22 +46,43 @@ public class Main_1865 {
 			int M = Integer.parseInt(st.nextToken()); //도로의 수
 			int W = Integer.parseInt(st.nextToken()); //웜홀의 수
 			
-			//도로의 정보
-			for(int i=0;i<M;i++) {
-				st = new StringTokenizer(br.readLine());
-				int MS = Integer.parseInt(st.nextToken()); //지점 번호1
-				int ME = Integer.parseInt(st.nextToken()); //지점 번호 2
-				int MT = Integer.parseInt(st.nextToken()); //이동 시간
+			dist = new int[N+1];
+			a = new ArrayList<>();
+			for(int i=0;i<=N;i++) {
+				a.add(new ArrayList<>());
 			}
 			
-			//웜홀의 정보
-			for(int i=0;i<W;i++) {
+			//도로의 정보 + 웜홀의 정보
+			for(int i=0;i<M+W;i++) {
 				st = new StringTokenizer(br.readLine());
-				int WS = Integer.parseInt(st.nextToken()); //시작 지점
-				int WE = Integer.parseInt(st.nextToken()); //도착 지점
-				int WT = Integer.parseInt(st.nextToken()); //줄어드는 시간
+				int start = Integer.parseInt(st.nextToken());
+				int end =Integer.parseInt(st.nextToken());
+				int weight = Integer.parseInt(st.nextToken());
+				
+				if(i<M) {
+					a.get(start).add(new Road(end, weight));
+					a.get(end).add(new Road(start, weight));
+				}else {
+					a.get(start).add(new Road(end, -weight));
+				}
+
 			}
+			
+			boolean isMinusCycle = false;
+			for(int i=1;i<=N;i++) {
+				if(bellmanFord(i)) { //만약 
+					isMinusCycle = true;
+					System.out.println("YES\n");
+					break;
+				}
+			}
+			
+			if(!isMinusCycle) {
+				System.out.println("NO\n");
+			}
+			
 		}
+		br.close();
 	}
 
 }
