8cfbd9d4833ba42903191f7f43d9186ee245cfd3
taeyeon0319 <xodus1349@likelion.org>
2024-09-20 17:30:48 +0900

    0920

--- algo/src/baekjoon/Main_1865.java
+++ algo/src/baekjoon/Main_1865.java
@@ -5,84 +5,93 @@ import java.util.*;
 public class Main_1865 {
 	static int N, M, W;
 	static int[] dist;
-	static ArrayList<ArrayList<Road>> a;
+	static List<int[]>[] graph;
 	static final int INF = 987654321;
 	
-	public class Road{
-		int end;
-		int weight;
-		
-		Road(int end, int weight){
-			this.end = end;
-			this.weight = weight;
-		}
-	}
-	//벨만포드 알고리즘
-	public static boolean bellmanFord(int start) {
-		Arrays.fill(dist,  INF);
-		dist[start] = 0;
-		boolean update = false;
-		
-		for(int i=1;i<N;i++) {
-			update=false;
-			
-			for(int j=1;j<=N;j++) {
-				for(Road road:a.get(j)) {
-					if(dist[j]!=INF && dist[road.end]>dist[j])
-				}
-//				https://steady-coding.tistory.com/91
-			}
-		}
-		return false;
-	}
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
 		int TC = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
 		for(int tc=0;tc<TC;tc++) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); //지점의 수
-			int M = Integer.parseInt(st.nextToken()); //도로의 수
-			int W = Integer.parseInt(st.nextToken()); //웜홀의 수
+			N = Integer.parseInt(st.nextToken()); //지점의 수
+			M = Integer.parseInt(st.nextToken()); //도로의 수
+			W = Integer.parseInt(st.nextToken()); //웜홀의 수
 			
 			dist = new int[N+1];
-			a = new ArrayList<>();
+			graph = new List[N+1] ;
 			for(int i=0;i<=N;i++) {
-				a.add(new ArrayList<>());
+				graph[i] = new ArrayList<>();
 			}
 			
-			//도로의 정보 + 웜홀의 정보
-			for(int i=0;i<M+W;i++) {
+			//도로 설정
+			for(int i=0;i<M;i++) {
 				st = new StringTokenizer(br.readLine());
-				int start = Integer.parseInt(st.nextToken());
-				int end =Integer.parseInt(st.nextToken());
-				int weight = Integer.parseInt(st.nextToken());
-				
-				if(i<M) {
-					a.get(start).add(new Road(end, weight));
-					a.get(end).add(new Road(start, weight));
-				}else {
-					a.get(start).add(new Road(end, -weight));
-				}
-
+				int s = Integer.parseInt(st.nextToken());
+				int e = Integer.parseInt(st.nextToken());
+				int t = Integer.parseInt(st.nextToken());
+				graph[s].add(new int[] {e, t});
+				graph[e].add(new int[] {s, t});
+			}
+			
+			//웜홀 설정
+			for(int i=0;i<W;i++) {
+				st = new StringTokenizer(br.readLine());
+				int S = Integer.parseInt(st.nextToken());
+				int E = Integer.parseInt(st.nextToken());
+				int T = Integer.parseInt(st.nextToken());
+				graph[S].add(new int[] {E, -T});
 			}
 			
 			boolean isMinusCycle = false;
+			
+			//만약 시간이 줄어들면서 출발 위치로 돌아온다면 YES!
 			for(int i=1;i<=N;i++) {
-				if(bellmanFord(i)) { //만약 
+				if(bellmanFord(i)) {
 					isMinusCycle = true;
-					System.out.println("YES\n");
+					System.out.println("YES");
 					break;
 				}
 			}
 			
+			//아니면 NO!
 			if(!isMinusCycle) {
-				System.out.println("NO\n");
+				System.out.println("NO");
 			}
-			
 		}
-		br.close();
+	}
+
+	private static boolean bellmanFord(int start) {
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
+		boolean update = false; //한번 돌았나?
+		
+		for(int i=1;i<N;i++) {
+			update = false;
+			for(int j=1;j<=N;j++) {
+				for(int[] g : graph[j]) {
+					if(dist[j] != INF && dist[g[0]] > dist[j] + g[1]) {
+						dist[g[0]] = dist[j]+g[1];
+						update=true;
+					}
+				}
+			}
+			if(!update) {
+				break;
+			}
+		}
+		
+		if(update) {
+			for(int i=1;i<=N;i++) {
+				for(int[] g: graph[i]) {
+					if(dist[i] != INF && dist[g[0]]>dist[i]+g[1]) {
+						return true; //한번 돌고 두번 돌았더니 결과가 바뀌었다?! 이건 음수가 있는거임
+					}
+				}
+			}
+		}
+		return false;
 	}
 
 }
